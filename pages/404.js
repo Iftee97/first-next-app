a9597747cd67a952ece0be2fbbe0785be4f27767
Iftee1711402 <tawsifhaque97@gmail.com>
2022-08-28 17:39:41 +0600

    added router redirect in the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from "next/link"
+import { useEffect } from "react"
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/') // redirects to the home page after 3 seconds
+    }, 3000)
+  }, [])
+
   return (
     <div className="not-found">
       <h1>Ooops...</h1>
