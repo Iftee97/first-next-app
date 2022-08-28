7be4f241c214dc0f0922788fc7ccd6dca3ca11a4
Iftee1711402 <tawsifhaque97@gmail.com>
2022-08-28 17:47:59 +0600

    added Home wrapper component for tititle and meta

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,19 @@
+import Head from "next/head"
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keyowrds' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+      </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className={styles.title}>Home</h1>
-      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat iure perferendis in assumenda laudantium iusto. Recusandae at praesentium repudiandae laudantium. Eligendi accusantium atque praesentium inventore iure quas vel corrupti reiciendis?</p>
-      <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat iure perferendis in assumenda laudantium iusto. Recusandae at praesentium repudiandae laudantium. Eligendi accusantium atque praesentium inventore iure quas vel corrupti reiciendis?</p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keyowrds' content='ninjas' />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home</h1>
+        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat iure perferendis in assumenda laudantium iusto. Recusandae at praesentium repudiandae laudantium. Eligendi accusantium atque praesentium inventore iure quas vel corrupti reiciendis?</p>
+        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat iure perferendis in assumenda laudantium iusto. Recusandae at praesentium repudiandae laudantium. Eligendi accusantium atque praesentium inventore iure quas vel corrupti reiciendis?</p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
 
