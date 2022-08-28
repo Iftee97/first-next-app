66413a8aefff3e682e40af2a141b63271889a1e5
Iftee1711402 <tawsifhaque97@gmail.com>
2022-08-28 17:36:04 +0600

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo iusto explicabo officia maiores sunt, expedita eveniet atque quia vero voluptatem nobis minima mollitia deserunt quidem accusantium natus? At, magnam!</p>
+    </div>
+  )
+}
+
+export default About
